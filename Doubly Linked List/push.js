2d8488faf8b43cd629d33264c94155f3845e895c
Harshvardhan <hsinha31@gmail.com>
2020-10-12 22:14:49 +0530

    doubly Linked List started

---
+++ Doubly Linked List/push.js
@@ -0,0 +1,43 @@
+// ************To add at end************
+
+class Node{
+    constructor(value){
+        this.val = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLL{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    
+    push(val){
+        let newNode = new Node(val);
+        if (this.head === null){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+
+        else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode; 
+        }
+
+    } 
+}
+
+let list = new DoublyLL;
+for(let i= 0; i<5;i++)
+list.push(i);
+
+
+
+
+
+
+
